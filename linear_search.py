25bacb9678a159768de6240cfd4780f9df66982c
snehamurgan <144421183+snehamurgan@users.noreply.github.com>
2023-10-10 12:17:22 +0530

    Update linear_search.py

--- linear_search.py
+++ linear_search.py
@@ -4,3 +4,14 @@ def linear_search_product(products, target_product):
         if products[i] == target_product:
             indices.append(i)
     return indices
+
+# Example usage
+product_list = ["Apple", "Banana", "Orange", "Apple", "Mango", "Apple"]
+target_product = "Apple"
+
+result = linear_search_product(product_list, target_product)
+
+if result:
+    print(f"The target product '{target_product}' was found at indices: {result}")
+else:
+    print(f"The target product '{target_product}' was not found in the list.")
